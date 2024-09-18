8198ce3afe739f6982d9c39c1df0805d59190484
mingg123 <spgb123spgb@gmail.com>
2024-09-18 17:03:25 +0900

    init commit

---
+++ onthego/sb-for-vsc/src/test/java/com/example/sb_for_vsc/SbForVscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_for_vsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbForVscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ onthego/src/test/java/com/onthego/onthego/OnthegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onthego.onthego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnthegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
