9ec16c5b8f7d1cb44b0c9f5753a564b479f6ff29
hanma556 <minyeong99089@naver.com>
2024-10-06 19:53:30 +0900

    init commit

---
+++ sb-for-vsc/src/main/java/com/example/sb_for_vsc/SbForVscApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_for_vsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbForVscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbForVscApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/onthego/onthego/OnthegoApplication.java
@@ -0,0 +1,14 @@
+package com.onthego.onthego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnthegoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World2!");
+		SpringApplication.run(OnthegoApplication.class, args);
+	}
+
+}
